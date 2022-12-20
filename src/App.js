aff6417b3e1efbb5dff3dc60b1d6f4f2355af78c
isai <isaiorellana.dev@gmail.com>
2022-12-20 14:12:52 -0600

    ✨ dev mode

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          El barrio mio
+          El barrio mio en dev
         </p>
         <a
           className="App-link"
